95856982f4f234e6b89fdeebab4ff9b8e451e0ea
Radu-Teodor Prosie <psn.radu@gmail.com>
2025-05-22 15:58:58 +0300

    Update Rheap.cpp
    ???? what was I cooking

--- src/Rheap.cpp
+++ src/Rheap.cpp
@@ -23,31 +23,6 @@ template <typename T, typename Compare = std::less<T>> class Rheap {
   std::mt19937 rng{std::random_device{}()};
   Compare comp{};
 
-  #include <array>
-#include <functional>
-#include <memory>
-#include <random>
-#include <stdexcept>
-#include <utility>
-
-template <typename T, typename Compare = std::less<T>> class Rheap {
-  class Node;
-  using NodePtr = std::shared_ptr<Node>;
-
-  // O(1) swap and top, O(log(heap_size)) all others (on average)
-
-  class Node {
-  public:
-    T value;
-    std::array<NodePtr, 2> child{};
-
-    explicit Node(const T &val) : value(val) {}
-  };
-
-  NodePtr root = nullptr;
-  std::mt19937 rng{std::random_device{}()};
-  Compare comp{};
-
   NodePtr merge(NodePtr a, NodePtr b) {
     if (!a || !b)
       return a ? a : b;
@@ -59,32 +34,6 @@ template <typename T, typename Compare = std::less<T>> class Rheap {
     a->child[0] = merge(a->child[0], b);
     return a;
   }
-
-public:
-  void push(const T &val) { root = merge(root, std::make_shared<Node>(val)); }
-
-  const T &top() const {
-    if (!root)
-      throw std::out_of_range("Heap is empty");
-    return root->value;
-  }
-
-  void pop() {
-    if (!root)
-      throw std::out_of_range("Heap is empty");
-    root = merge(root->child[0], root->child[1]);
-  }
-
-  [[nodiscard]] bool empty() const { return !root; }
-
-  void swap(Rheap &other) noexcept { std::swap(root, other.root); }
-
-  void meld(Rheap &other) {
-    root = merge(root, other.root);
-    other.root.reset();
-  }
-};
-
 
 public:
   void push(const T &val) { root = merge(root, std::make_shared<Node>(val)); }
